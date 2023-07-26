946ca9e4a76cdbc0ca5b74b2d567699f22eb5b4c
CarlosMoretto2 <carlos.morettobr2@gmail.com>
2023-07-26 08:47:11 -0300

    Inicio Funções

---
+++ object.js
@@ -0,0 +1,19 @@
+// JSON -> Javascript Object notation
+// Formato textual de objetos diferente do objeto em js
+
+//Objeto conjunto chave e valor
+
+const prod1 = {}
+prod1.nome = 'Celular'
+prod1.preco = 2499.99
+prod1['Desconto'] = 0.40 //Evitar atributos com espaço
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa',
+    preco: 19.90,
+
+}
+
+console.log(prod2)
